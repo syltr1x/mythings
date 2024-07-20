c9c1fbb4d3a1e145a053b7d49216dc680d4f058f
syltr1x <syltr1x@proton.me>
2024-07-19 23:33:49 -0300

    add: help menu, fetch option, pull option

--- giter.sh
+++ giter.sh
@@ -13,57 +13,90 @@ gray="\e[0;37m\033[1m"
 # Variables
 dirList=(".")
 validDirs=()
-params=("-maxdepth 1")
+
+
+help_menu() {
+	echo "AYUDA\nMENU"
+}
+
+obtain_dirs() {
+	if [ $# -eq 1 ]; then
+		files_list=$(find . $1 -type d)
+	else
+		files_list=$(find . -type d)
+	fi
+	# Directory List
+	while IFS= read -r linea; do
+		firstChar=${linea:0:1}
+		linea=$(awk '{print $NF}' <<< "$linea")
+		if [ "$linea" != "." ] && [ "$linea" != ".." ]; then
+			dirList+=("$linea")
+		fi
+	done <<< "$files_list"
+
+	# Directory Scan
+	for dir in ${dirList[@]}; do
+		if [ -d "$dir/.git" ]; then
+			validDirs+=("$dir")
+		fi
+	done
+	echo ${validDirs[@]}
+}
+giter() {
+	validDirs=$(obtain_dirs $dirParams)
+	giter $hidden $recursive $pull $fetch
+	if [ ! $recursive ]; then
+		dirParams="-maxdepth 1"
+	fi
+	if [ $pull ]; then
+		gitComm="pull"
+	elif [ $fetch ]; then	
+		gitComm="fetch"
+	else
+		gitComm="status -s"
+	fi
+	# Obtain Git Data
+	for repo in ${validDirs[@]}; do
+		gitCount=$(git -C $repo status -s | wc -l)
+		if [ "$repo" == "." ]; then
+			echo -e "${purple}[*] Directory: ${end}${gray}${repo/./$(basename "$PWD")}${end} (current dir)"
+		else
+			if [ $gitCount -gt 0 ]; then
+				echo -e "${turquoise}[*] Directory: ${end}${gray}$repo${end}"
+			else
+				echo -e "${blue}[*] Directory: ${end}${gray}$repo${end} (no changes)"
+			fi
+		fi
+		if [ $gitCount -gt 0 ]; then
+			git -C $repo $gitComm
+		fi
+		commitId=$(git -C $repo log --oneline | head -n1 | awk ' { print $1 } ')
+		commitDesc=$(git -C $repo log --oneline | head -n1 | cut -d\  -f2- )
+		echo -e "($commitId) '$commitDesc'"
+		echo ""
+	done
+}
+
+# Initial Variables
+display_help=false
+hidden=false
+recursive=false
+pull=false
+fetch=false
 
 # Parameters
-while getopts ":h :r" arg; do
+while getopts ":h :a :r :p :pr :f :fr" arg; do
 	case $arg in
-		h) params+=("-maxdepth 1");;
-		r) params=("");;
+		h) display_help=true;;
+		a) hidden=true;; 
+		r) recursive=true;;
+		p) pull=true;;
+		f) fetch=true;;
 	esac
 done
-
-# Directory List
-while IFS= read -r linea; do
-	firstChar=${linea:0:1}
-	linea=$(awk '{print $NF}' <<< "$linea")
-	if [ "$linea" != "." ] && [ "$linea" != ".." ]; then
-		dirList+=("$linea")
-	fi
-done < <(find . ${params[@]} -type d)
-
-# Directory Scan
-for dir in ${dirList[@]}; do
-	if [ -d "$dir/.git" ]; then
-		validDirs+=("$dir")
-		# if [ ! -f "$dir/.history_changes" ]; then
-		# 	echo "[!] '.history_changes' not found in $dir. Consider execute giter -p(pull) or -f(fetch) to obtain last changes data."
-		# 	datetime=$(date | awk '{NF=""}1')
-		# 	echo -e "$datetime -> add: '.history_changes to .gitignore (not commited)" > $dir/.history_changes
-		# 	echo ".history_changes" >> $dir/.gitignore
-		# fi
-	fi
-done
-
-# Obtain Git Data
-for repo in ${validDirs[@]}; do
-	gitCount=$(git -C $repo status -s | wc -l)
-	# lastChanges=$(cat $repo/.history_changes | tail -1)
-	# echo -e "Last changes: $lastChanges"
-	if [ "$repo" == "." ]; then
-		echo -e "${purple}[*] Directory: ${end}${gray}${repo/./$(basename "$PWD")}${end} (current dir)"
-	else
-		if [ $gitCount -gt 0 ]; then
-			echo -e "${turquoise}[*] Directory: ${end}${gray}$repo${end}"
-		else
-			echo -e "${blue}[*] Directory: ${end}${gray}$repo${end} (no changes)"
-		fi
-	fi
-	if [ $gitCount -gt 0 ]; then
-		git -C $repo status -s
-	fi
-	commitId=$(git -C $repo log --oneline | head -n1 | awk ' { print $1 } ')
-	commitDesc=$(git -C $repo log --oneline | head -n1 | cut -d\  -f2- )
-	echo -e "($commitId) '$commitDesc'"
-	echo ""
-done
+echo $display_help
+# if [ $display_help == true ]; then
+# 	help_menu
+# else
+# 	giter $hidden $recursive $pull $fetch
+# fi
